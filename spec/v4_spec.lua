4b5cf8430e47a1efa67ad79a3cc6c24f41db2074
James Callahan <james@konghq.com>
2018-08-28 16:48:07 +1000

    spec/: Update to newer AWSv4 test suite

--- spec/v4_spec.lua
+++ spec/v4_spec.lua
@@ -1,9 +1,9 @@
 local awsv4 = require "aws.v4"
 
 describe("Pass AWSv4 test suite", function()
-	-- The test suite was obtained from http://docs.aws.amazon.com/general/latest/gr/samples/aws4_testsuite.zip
+	-- The test suite was obtained from https://docs.aws.amazon.com/general/latest/gr/samples/aws-sig-v4-test-suite.zip
+	-- sha1sum of the test suite .zip is c66ea06884dad4348558e93380c8bf91e8551a6f
 	-- Information about it can be found at http://docs.aws.amazon.com/general/latest/gr/signature-v4-test-suite.html
-
 	local function read_file(path)
 		local fd, err, code = io.open(path, "rb")
 		if fd == nil then
@@ -18,72 +18,68 @@ describe("Pass AWSv4 test suite", function()
 		return contents
 	end
 
-	local dir = "./spec/aws4_testsuite/"
+	local dir = "./spec/aws-sig-v4-test-suite/"
 	for _, test_name in ipairs {
 		"get-header-key-duplicate";
+		"get-header-value-multiline";
 		"get-header-value-order";
 		"get-header-value-trim";
-		"get-relative-relative";
-		"get-relative";
-		"get-slash-dot-slash";
-		"get-slash-pointless-dot";
-		"get-slash";
-		"get-slashes";
-		"get-space";
 		"get-unreserved";
 		"get-utf8";
+		"get-vanilla";
 		"get-vanilla-empty-query-key";
-		"get-vanilla-query-order-key-case";
+		"get-vanilla-query";
 		"get-vanilla-query-order-key";
+		"get-vanilla-query-order-key-case";
 		"get-vanilla-query-order-value";
 		"get-vanilla-query-unreserved";
-		"get-vanilla-query";
-		"get-vanilla-ut8-query";
-		"get-vanilla";
+		"get-vanilla-utf8-query";
+		"normalize-path/get-relative";
+		"normalize-path/get-relative-relative";
+		"normalize-path/get-slash";
+		"normalize-path/get-slash-dot-slash";
+		"normalize-path/get-slashes";
+		"normalize-path/get-slash-pointless-dot";
+		"normalize-path/get-space";
 		"post-header-key-case";
 		"post-header-key-sort";
 		"post-header-value-case";
-		"post-vanilla-empty-query-value";
-		-- These two have invalid whitespace in targets, so skip them
-		--"post-vanilla-query-nonunreserved";
-		--"post-vanilla-query-space";
-		"post-vanilla-query";
+		"post-sts-token/post-sts-header-after";
+		"post-sts-token/post-sts-header-before";
 		"post-vanilla";
-		"post-x-www-form-urlencoded-parameters";
+		"post-vanilla-empty-query-value";
+		"post-vanilla-query";
 		"post-x-www-form-urlencoded";
+		"post-x-www-form-urlencoded-parameters";
 	} do
-		local req = read_file(dir..test_name..".req")
-		local creq = read_file(dir..test_name..".creq")
-		local sts = read_file(dir..test_name..".sts")
-		local authz = read_file(dir..test_name..".authz")
-		-- local sreq = read_file(dir..test_name..".sreq")
-		it("passes test #" .. test_name:gsub("%-", "_"), function()
-			-- http (lowercase) is not allowed... but amazon use it
-			local method, target, str_headers, body = req:match("^(%S+) (.-) [Hh][Tt][Tt][Pp]/1.[01]\r\n(.-\r\n)\r\n(.*)")
+		local filename = test_name:match("[^/]+$")
+		local req = read_file(dir..test_name.."/"..filename..".req")
+		;(req and it or pending)("passes test #" .. test_name:gsub("%-", "_"), function()
+			local creq = read_file(dir..test_name.."/"..filename..".creq")
+			local sts = read_file(dir..test_name.."/"..filename..".sts")
+			local authz = read_file(dir..test_name.."/"..filename..".authz")
+			-- local sreq = read_file(dir..test_name..".sreq")
+
+			local method, target, pos = req:match("^(%S+) (.-) HTTP/1.[01]()")
 			local path, query = target:match("([^%?]*)%??(.*)")
 			path = path or target
+			local str_headers, body = req:match("^\n(.-\n)\n(.*)", pos)
+			str_headers = str_headers or req:sub(pos+1) .. "\n"
 			local headers = {
 				["X-Amz-Date"] = false; -- test suite uses normal date header instead
 			}
-			for k, v in str_headers:gmatch("([^:]*): ?(.-)\r\n") do
+			for k, v in str_headers:gmatch("([^%s:]+): *(.-)\n%f[%z%w]") do
+				v = v:gsub("\n +", ",")
 				local old_v = headers[k:lower()]
 				if old_v then
-					-- The amazon testsuite seems to sort headers when the spec says it shouldn't
-					-- As a hack, sort them here, so that tests pass
-					local t = {}
-					for val in old_v:gmatch("[^,]+") do
-						t[#t+1] = val
-					end
-					t[#t+1] = v
-					table.sort(t)
-					v = table.concat(t, ",")
+					v = old_v .. "," .. v
 				end
 				headers[k:lower()] = v
 			end
 			if body == "" then body = nil end
 			local http_req, interim = awsv4.prepare_request {
 				Region = "us-east-1";
-				Service = "host";
+				Service = "service";
 				AccessKey = "AKIDEXAMPLE";
 				SecretKey = "wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY";
 				method = method;
@@ -91,11 +87,11 @@ describe("Pass AWSv4 test suite", function()
 				query = query;
 				headers = headers;
 				body = body;
-				timestamp = 1315611360; -- Timestamp used by all tests
+				timestamp = 1440938160; -- Timestamp used by all tests
 			}
 			assert.truthy(http_req)
-			assert.same(creq:gsub("\r\n", "\n"), interim.CanonicalRequest)
-			assert.same(sts:gsub("\r\n", "\n"), interim.StringToSign)
+			assert.same(creq, interim.CanonicalRequest)
+			assert.same(sts, interim.StringToSign)
 			assert.same(authz, interim.Authorization)
 		end)
 	end
